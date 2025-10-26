d97d038f47851b64198a09e95e98ad20e141cd73
Oscar Bennich-Björkman <oscar.bennich@gmail.com>
2025-10-26 22:14:29 +0100

    Add temporary work in progress banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ function App() {
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-900 mb-4">
-          Hello World
+          🚧 WORK IN PROGRESS 🚧
         </h1>
         <p className="text-lg text-gray-600">
-          Welcome to my personal site TEST
+          <iframe src="https://giphy.com/embed/S13KR45aV7l5kJuenU" width="480" height="269" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
         </p>
       </div>
     </div>
