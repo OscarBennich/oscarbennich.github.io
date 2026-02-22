2a752c0101c084f59237ac89bbee4a59468bd8a8
Oscar Bennich-Björkman <oscar.bennich@gmail.com>
2026-02-22 21:50:28 +0100

    Change footer text/link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,15 @@ function Footer(): React.ReactElement {
   return (
     <footer className="relative z-10 bg-gray-800 border-t border-gray-700">
       <div className="max-w-6xl mx-auto px-4 py-6">
-        <p className="text-center text-gray-400 font-mono text-sm">
-          Curious how I built this site?{' '}
+        <p className="text-center font-mono text-sm">
           <a
             href="https://github.com/OscarBennich/oscarbennich.github.io/blob/main/README.md"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-400 hover:underline"
           >
-            Read more here
-          </a>.
+            Curious how I built this site?{' '}
+          </a>
         </p>
       </div>
     </footer>
