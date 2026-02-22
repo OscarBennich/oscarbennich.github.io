2e1a5af1c7fc580b88190e8927bc3a9695190bb5
Oscar Bennich-Björkman <oscar.bennich@gmail.com>
2026-02-22 22:06:56 +0100

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,24 @@ function Footer(): React.ReactElement {
   return (
     <footer className="relative z-10 bg-gray-800 border-t border-gray-700">
       <div className="max-w-6xl mx-auto px-4 py-6">
-        <p className="text-center font-mono text-sm">
+        <p className="text-center text-gray-400 font-mono text-sm">
+          Wondering how this site was built?
+        </p>
+        <p className="text-center text-gray-400 font-mono text-sm">
+          You can read about it {" "}
           <a
             href="https://github.com/OscarBennich/oscarbennich.github.io/blob/main/README.md"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-400 hover:underline"
           >
-            Curious how I built this site?{' '}
+            here
           </a>
+          .
         </p>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
